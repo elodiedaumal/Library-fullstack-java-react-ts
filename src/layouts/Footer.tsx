29c69fa40a506f269fc7e6c25939b46bcdaafae0
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-12-04 16:32:51 +0100

    correction of marging in footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -10,12 +10,10 @@ const Footer: React.FC = () => {
    return (
       <div className='bg-sky-700 '>
          <footer className='mx-auto px-10 flex flex-wrap justify-between items-center py-5'>
-            <p className='col-md-4 mb-0 text-white'>
-               &copy; Example Library App, Inc
-            </p>
-            <ul className='flex'>
+            <p className=' text-white'>&copy; Example Library App, Inc</p>
+            <ul className='flex gap-4'>
                {NavbarLinks.map((navlink: NavLink) => (
-                  <li className='ml-4' key={navlink.id}>
+                  <li className='' key={navlink.id}>
                      <a href={navlink.url} className='text-white'>
                         {navlink.name}
                      </a>
